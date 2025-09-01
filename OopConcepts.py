5a457b73ae76f9bc7a9e0beffcbf1a2974689c39
Atikur Rahman <atikur2013rahman@gmail.com>
2025-09-01 22:07:52 +0600

    OOP

---
+++ OopConcepts.py
@@ -0,0 +1,17 @@
+#Create a Class
+class MyClass:
+  x = 5
+
+p1 = MyClass()
+print(p1.x)
+
+#The __init__() Method
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("John", 36)
+
+print(p1.name)
+print(p1.age)
