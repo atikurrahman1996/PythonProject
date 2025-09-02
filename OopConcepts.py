ad5904327ffadc2e0188126db7df6aef52ce3d74
Atikur Rahman <atikur2013rahman@gmail.com>
2025-09-02 10:01:06 +0600

    OOP

--- OopConcepts.py
+++ OopConcepts.py
@@ -1,6 +1,6 @@
 #Create a Class
 class MyClass:
-  x = 5
+  x = 25
 
 p1 = MyClass()
 print(p1.x)
@@ -11,7 +11,36 @@ class Person:
     self.name = name
     self.age = age
 
-p1 = Person("John", 36)
+p1 = Person("Atik", 29)
 
 print(p1.name)
 print(p1.age)
+
+#The __str__() Method
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def __str__(self):
+    return f"{self.name} {self.age}"
+
+p1 = Person("Atik", 29)
+
+print(p1)
+
+#Create Methods
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+
+p1 = Person("John", 36)
+p1.myfunc()
+
+
---
+++ OopInheritance.py
@@ -0,0 +1,66 @@
+'''
+Create a class named Person, with firstname and lastname properties, and a printname method:
+
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+#Use the Person class to create an object, and then execute the printname method:
+
+x = Person("John", "Doe")
+x.printname()
+
+#child class
+
+class Student(Person):
+  pass
+
+x = Student("Mike", "Olsen")
+x.printname()
+
+'''
+
+
+
+#parent class
+
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+#child class
+
+class Student(Person):
+  def __init__(self, fname, lname):
+    Person.__init__(self, fname, lname)
+
+x = Student("AR", "ATIK")
+x.printname()
+
+'''
+super() function that will make the child class inherit all the methods and properties from its parent:
+
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person):
+  def __init__(self, fname, lname, year):
+    super().__init__(fname, lname)
+    self.graduationyear = year
+
+x = Student("Mike", "Olsen", 2019)
+print(x.graduationyear)
+'''
