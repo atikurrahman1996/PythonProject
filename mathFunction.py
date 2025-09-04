f763d0f4d0dd2754c7fcb5e63c1d26c9ec8ebbff
Atikur Rahman <atikur2013rahman@gmail.com>
2025-09-04 09:39:26 +0600

    module functions

---
+++ mathFunction.py
@@ -0,0 +1,24 @@
+
+#Built-in Math Functions
+
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+
+#The abs() function returns the absolute (positive) value
+
+x = abs(-7.25)
+
+print(x)
+
+# power function
+x = pow(4, 3)
+print(x)
+
+#Math Module
+
+import math
+x = math.sqrt(64)
+print(x)
