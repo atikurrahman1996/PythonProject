7fe27770d54a8244becf95000d09a137100b0e02
Atikur Rahman <atikur2013rahman@gmail.com>
2025-09-08 09:28:23 +0600

    Day3 operators

--- Day3_Operators/operators.py
+++ Day3_Operators/operators.py
@@ -28,7 +28,7 @@ b = 2 # b is a variable name and 3 is an integer data type
 # Arithmetic operations and assigning the result to a variable
 total = a + b
 diff = a - b
-product = a * b
+multiply = a * b
 division = a / b
 remainder = a % b
 floor_division = a // b
@@ -37,8 +37,8 @@ exponential = a ** b
 # I should have used sum instead of total but sum is a built-in function try to avoid overriding builtin functions
 print(total) # if you don't label your print with some string, you never know from where is  the result is coming
 print('a + b = ', total)
-print('a - b = ', diff)
-print('a * b = ', product)
+print('a - b = ', diff)  #difference
+print('a * b = ', multiply)
 print('a / b = ', division)
 print('a % b = ', remainder)
 print('a // b = ', floor_division)
@@ -51,14 +51,14 @@ num_two = 4
 # Arithmetic operations
 total = num_one + num_two
 diff = num_two - num_one
-product = num_one * num_two
+multiply = num_one * num_two
 div = num_two / num_two
 remainder = num_two % num_one
 
 # Printing values with label
 print('total: ', total)
 print('difference: ', diff)
-print('product: ', product)
+print('multi: ', multiply)
 print('division: ', div)
 print('remainder: ', remainder)
 
