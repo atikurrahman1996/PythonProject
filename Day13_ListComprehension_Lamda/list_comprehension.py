7f60406270dd8c557661edbe235ada53834beefb
Atikur Rahman <atikur2013rahman@gmail.com>
2025-09-24 10:04:03 +0600

    Day20 higher order

---
+++ Day13_ListComprehension_Lamda/list_comprehension.py
@@ -0,0 +1,55 @@
+#List comprehension in Python is a compact way of creating a list from a sequence. It is a short way to create a new list.
+#List comprehension is considerably faster than processing a list using the for loop.
+
+#Example:1
+
+#if we want to change a string to a list of characters. We can use a couple of methods.
+
+# One way - without list comprehension
+language = 'Python'
+lst = list(language) # changing the string to list
+print(type(lst))     # list
+print(lst)           # ['P', 'y', 't', 'h', 'o', 'n']
+
+# Second way: list comprehension
+lst = [i for i in language]
+print(type(lst)) # list
+print(lst)       # ['P', 'y', 't', 'h', 'o', 'n']
+
+
+#Example:2
+
+#generate a list of numbers
+
+# Generating numbers
+numbers = [i for i in range(11)]  # to generate numbers from 0 to 10
+print(numbers)                    # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# It is possible to do mathematical operations during iteration
+squares = [i * i for i in range(11)]
+print(squares)                    # [0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+# It is also possible to make a list of tuples
+numbers = [(i, i * i) for i in range(11)]
+print(numbers)
+
+#Example:2
+#List comprehension can be combined with if expression
+
+# Generating even numbers
+even_numbers = [i for i in range(21) if i % 2 == 0]  # to generate even numbers list in range 0 to 21
+print(even_numbers)                    # [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+
+# Generating odd numbers
+odd_numbers = [i for i in range(21) if i % 2 != 0]  # to generate odd numbers in range 0 to 21
+print(odd_numbers)                      # [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+
+# Filter numbers: let's filter out positive even numbers from the list below
+numbers = [-8, -7, -3, -1, 0, 1, 3, 4, 5, 7, 6, 8, 10]
+positive_even_numbers = [i for i in numbers if i % 2 == 0 and i > 0]
+print(positive_even_numbers)                    # [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+
+# Filter numbers: let's filter out odd numbers from the list below
+numbers2 = [-8, -7, -3, -1, 0, 1, 3, 4, 5, 7, 6, 8, 10]
+neg_odd_numbers = [i for i in numbers2 if i % 2 !=0 ]
+print(neg_odd_numbers)
---
+++ Day20_Higher_order_function/higherorder.py
@@ -0,0 +1,38 @@
+#Function as a Parameter
+def higher_order_function(f, lst):  # function as a parameter
+    summation = f(lst)
+    return summation
+
+# Passing built-in sum function and a list
+result = higher_order_function(sum, [1, 2, 3, 4, 5])
+print(result)
+
+
+#Function as a Return Value
+
+def square(x):          # a square function
+    return x ** 2
+
+def cube(x):            # a cube function
+    return x ** 3
+
+def absolute(x):        # an absolute value function
+    if x >= 0:
+        return x
+    else:
+        return -(x)
+
+def higher_order_function(type): # a higher order function returning a function
+    if type == 'square':
+        return square
+    elif type == 'cube':
+        return cube
+    elif type == 'absolute':
+        return absolute
+
+result = higher_order_function('square')
+print(result(3))       # 9
+result = higher_order_function('cube')
+print(result(3))       # 27
+result = higher_order_function('absolute')
+print(result(-3))      # 3
